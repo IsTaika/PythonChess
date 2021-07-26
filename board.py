2adc1f2c4fb1f141adf17a15dba3c6dcfd9e4443
IsTaika <MM0Dnbp1EFmN3qVRZs9f>
2021-07-26 20:19:48 +0300

    bugs added

--- board.py
+++ board.py
@@ -9,7 +9,7 @@ import pygame
 
 
 class Board:
-    def __init__(self, rows, lines):
+    def __init__(self, rows, lines, mode):
         self.rows = rows
         self.lines = lines
         self.board = [[0 for i in range(8)] for _ in range(rows)]
@@ -50,6 +50,8 @@ class Board:
         self.board[6][6] = Pawn("white", "pawn", 6, 6)
         self.board[6][7] = Pawn("white", "pawn", 6, 7)
 
+        self.mode = mode
+
     def update_moves(self):
         for i in range(self.lines):
             for j in range(self.rows):
@@ -65,3 +67,60 @@ class Board:
                         danger_moves.append(danger)
 
         return danger_moves
+
+    def check(self, color):
+        self.update_moves()
+        d_moves = self.danger_moves(color)
+        king = (-1, -1)
+        for i in range(self.lines):
+            for j in range(self.rows):
+                if self.board[i][j] != 0:
+                    if (self.board[i][j].type == 'king') and (self.board[i][j] == color):
+                        king = (i, j)
+        if king in d_moves:
+            return True
+        else:
+            return False
+
+    def select(self, line, row, color):
+        change = False
+        pos = (-1, -1)
+        for i in range(self.lines):
+            for j in range(self.rows):
+                if self.board[i][j] != 0:
+                    if self.board[i][j].selected:
+                        pos = (i, j)
+
+        if self.board[line][row] == 0 and pos != (-1, -1):
+            move = self.board[pos[0]][pos[1]].move_l
+            if (line, row) in move:
+                change = self.move(pos, (line, row), color)
+        else:
+            if pos == (-1, -1):
+                self.reset_select()
+
+    def reset_select(self):
+        for i in range(self.lines):
+            for j in range(self.rows):
+                if self.board[i][j] != 0:
+                    self.board.seleted == False
+
+    def move(self, start, end, color):
+        cheked = self.check(color)
+        change = True
+        newBoard = self.board
+        newBoard[start[0]][start[1]].change_position((end[0], end[1]))
+        newBoard[end[0]][end[1]] = newBoard[start[0]][start[1]]
+        newBoard[start[0]][start[1]] = 0
+        self.board = newBoard
+        if self.check(color) or (cheked and self.check(color)):
+            change = False
+            newBoard = self.board
+            newBoard[end[0]][end[1]].change_position((start[0], start[1]))
+            newBoard[start[0]][start[1]] = newBoard[end[0]][end[1]]
+            newBoard[end[0]][end[1]] = 0
+            self.board = newBoard
+        else:
+            self.reset_select()
+        self.update_moves()
+        return change
